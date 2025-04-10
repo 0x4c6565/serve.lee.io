433c96b9051f2efabf5976195e65cd60cc00945a
Lee Spottiswood <me@lee.io>
2025-04-10 23:37:09 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/0x4c6565/serve.lee.io
+
+go 1.24.0
+
+require github.com/spf13/pflag v1.0.6 // indirect
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/spf13/pflag"
+)
+
+func main() {
+	port := pflag.IntP("port", "p", 8080, "Port to serve on")
+	directory := pflag.StringP("directory", "d", ".", "Directory to serve")
+	pflag.Parse()
+
+	fs := http.FileServer(http.Dir(*directory))
+	http.Handle("/", fs)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
